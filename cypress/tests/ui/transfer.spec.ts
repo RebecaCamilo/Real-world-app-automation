48039c86f80db5d5b2537a93d623fc917713f53e
RebecaCamilo <rebecacamilo3@gmail.com>
2024-05-25 22:50:39 -0300

    add transfer unit tests

--- cypress/tests/ui/transfer.spec.ts
+++ cypress/tests/ui/transfer.spec.ts
@@ -1,28 +1,31 @@
 import userData from "../../fixtures/users/userData.json"
 import LoginPage from "../../pages/loginPage"
+import TransferPage from "../../pages/transferPage"
 
-const loginPage = new LoginPage()
+const loginPage = new LoginPage();
+const transferPage = new TransferPage();
 
 
 describe('Transaction RWA Tests', () => {
-        
-    describe('Enviar dinheiro com saldo suficiente', () => {
-    it('Deve enviar dinheiro com sucesso', () => {
-        // Login
+
+    beforeEach(() => {
+        // Realizar login antes de cada teste de transação
         loginPage.accessLoginPage();
         loginPage.loginWithUsernameAndPassword(userData.userSuccess.username, userData.userSuccess.password);
-        cy.location('pathname').should('equal', '/');    
-
-
-    });
+        cy.location('pathname').should('equal', '/'); 
     });
 
-    describe('Enviar dinheiro com saldo insuficiente', () => {
-        it('Deve exibir mensagem de erro ao enviar dinheiro sem saldo suficiente', () => {
-          // Login
-        loginPage.accessLoginPage();
-        loginPage.loginWithUsernameAndPassword(userData.userSuccess.username, userData.userSuccess.password);
-        cy.location('pathname').should('equal', '/');    
-        });
+    it.only('Enviar dinheiro com saldo suficiente', () => {
+        transferPage.accessTransactionPage();
+        transferPage.clickFirstUser();
+        // transactionPage.sendMoneyWithSufficientBalance();
+        // transactionPage.checkTransactionSuccess();
     });
+
+    it('Enviar dinheiro com saldo insuficiente', () => {
+        // transactionPage.accessTransactionPage();
+        // transactionPage.sendMoneyWithInsufficientBalance();
+        // transactionPage.checkTransactionFailure();
+    });
+
 })
